3fbbbd1e83b44e1a49a66245cd451b32a591b463
Eike Ziller <git@eikeziller.de>
2017-04-22 21:25:04 +0200

    Start with dummy plugin

---
+++ plugins/haskell/haskell.pro
@@ -0,0 +1,36 @@
+DEFINES += HASKELL_LIBRARY
+
+# Haskell files
+
+SOURCES += haskellplugin.cpp
+
+HEADERS += haskellplugin.h \
+        haskell_global.h \
+        haskellconstants.h
+
+## uncomment to build plugin into user config directory
+## <localappdata>/plugins/<ideversion>
+##    where <localappdata> is e.g.
+##    "%LOCALAPPDATA%\QtProject\qtcreator" on Windows Vista and later
+##    "$XDG_DATA_HOME/data/QtProject/qtcreator" or "~/.local/share/data/QtProject/qtcreator" on Linux
+##    "~/Library/Application Support/QtProject/Qt Creator" on OS X
+#USE_USER_DESTDIR = yes
+
+###### If the plugin can be depended upon by other plugins, this code needs to be outsourced to
+###### <dirname>_dependencies.pri, where <dirname> is the name of the directory containing the
+###### plugin's sources.
+
+QTC_PLUGIN_NAME = Haskell
+QTC_LIB_DEPENDS += \
+    # nothing here at this time
+
+QTC_PLUGIN_DEPENDS += \
+    coreplugin
+
+QTC_PLUGIN_RECOMMENDS += \
+    # optional plugin dependencies. nothing here at this time
+
+###### End _dependencies.pri contents ######
+
+include(config.pri)
+include($$IDE_SOURCE_TREE/src/qtcreatorplugin.pri)
---
+++ plugins/haskell/haskellplugin.cpp
@@ -0,0 +1,74 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#include "haskellplugin.h"
+#include "haskellconstants.h"
+
+namespace Haskell {
+namespace Internal {
+
+HaskellPlugin::HaskellPlugin()
+{
+    // Create your members
+}
+
+HaskellPlugin::~HaskellPlugin()
+{
+    // Unregister objects from the plugin manager's object pool
+    // Delete members
+}
+
+bool HaskellPlugin::initialize(const QStringList &arguments, QString *errorString)
+{
+    // Register objects in the plugin manager's object pool
+    // Load settings
+    // Add actions to menus
+    // Connect to other plugins' signals
+    // In the initialize function, a plugin can be sure that the plugins it
+    // depends on have initialized their members.
+
+    Q_UNUSED(arguments)
+    Q_UNUSED(errorString)
+
+    return true;
+}
+
+void HaskellPlugin::extensionsInitialized()
+{
+    // Retrieve objects from the plugin manager's object pool
+    // In the extensionsInitialized function, a plugin can be sure that all
+    // plugins that depend on it are completely initialized.
+}
+
+ExtensionSystem::IPlugin::ShutdownFlag HaskellPlugin::aboutToShutdown()
+{
+    // Save settings
+    // Disconnect from signals that are not needed during shutdown
+    // Hide UI (if you add UI that is not in the main window directly)
+    return SynchronousShutdown;
+}
+
+} // namespace Internal
+} // namespace Haskell
